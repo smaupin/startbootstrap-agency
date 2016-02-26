bd85b289d300c416d763a24bf58b0c60198966f5
smaupin <scotmaupin@gmail.com>
2016-02-26 03:45:55 -0800

    added SYI and description but links are not real yet.

--- js/agency.js
+++ js/agency.js
@@ -18,9 +18,46 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+// For Several Years in Color Switching between variations.
+$('#CP01-200').click(function() {
+	$('#CP01-800').show();
+	$('#CP02-800').hide();
+	$('#CP03-800').hide();
+	$('#CP04-800').hide();
+	$('#CP05-800').hide();
+});
+$('#CP02-200').click(function() {
+	$('#CP02-800').show();
+	$('#CP01-800').hide();
+	$('#CP03-800').hide();
+	$('#CP04-800').hide();
+	$('#CP05-800').hide();
+});
+$('#CP03-200').click(function() {
+	$('#CP03-800').show();
+	$('#CP02-800').hide();
+	$('#CP01-800').hide();
+	$('#CP04-800').hide();
+	$('#CP05-800').hide();
+});
+$('#CP04-200').click(function() {
+	$('#CP04-800').show();
+	$('#CP02-800').hide();
+	$('#CP03-800').hide();
+	$('#CP01-800').hide();
+	$('#CP05-800').hide();
+});
+$('#CP05-200').click(function() {
+	$('#CP05-800').show();
+	$('#CP02-800').hide();
+	$('#CP03-800').hide();
+	$('#CP04-800').hide();
+	$('#CP01-800').hide();
 });
